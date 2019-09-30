5bd37567f48d490630e2c9b345ec118fc4d9484c
Daniel Weller <daniel.weller@softwarepark.cc>
2019-09-30 14:29:08 +0200

    Started moving to streaming toClauses.

--- src/main/scala/maxsat/toClauses.scala
+++ src/main/scala/maxsat/toClauses.scala
@@ -4,26 +4,23 @@ import akka.NotUsed
 import akka.stream.scaladsl.Source
 
 object toClauses {
-  def apply[T](f: Formula[T]): Source[Clause[T], NotUsed] =
-    Source(rec(f))
-
-  def rec[T](f: Formula[T]): Set[Clause[T]] = {
+  def apply[T](f: Formula[T]): Source[Clause[T], NotUsed] = {
     f match {
-      case a @ Atom(_) => Set(Clause(negative = Set(), positive = Set(a)))
-      case And(conjuncts) => conjuncts.flatMap( f => rec(f) ).toSet
-      case Eq(left, right) => rec(And(Seq(Imp(left, right), Imp(right, left))))
-      case Imp(left, right) => rec(Or(Seq(Not(left), right)))
-      case Not( a @ Atom(_)) => Set(Clause(negative = Set(a), positive = Set()))
-      case Not(Not(g)) => rec(g)
-      case Not(Or(disjuncts)) => rec(And(disjuncts.map(Not(_))))
-      case Not(And(conjuncts)) => rec(Or(conjuncts.map(Not(_))))
-      case Not(Imp(left, right)) => rec(And(Seq(left, Not(right))))
-      case Not(Eq(left, right)) => rec(Not(And(Seq(Imp(left, right), Imp(right, left)))))
-      case Or(disjuncts) => merge(disjuncts.map(d => rec(d)))
+      case a @ Atom(_) => Source.fromIterator(() => Set(Clause(Set[Atom[T]](), Set(a))).iterator)
+      case And(conjuncts) => Source.fromIterator(() => conjuncts.iterator).flatMapMerge(2, f => apply(f) )
+      case Eq(left, right) => apply(And(Seq(Imp(left, right), Imp(right, left))))
+      case Imp(left, right) => apply(Or(Seq(Not(left), right)))
+      case Not( a @ Atom(_)) => Source.fromIterator(() => Set(Clause(Set(a), Set[Atom[T]]())).iterator)
+      case Not(Not(g)) => apply(g)
+      case Not(Or(disjuncts)) => apply(And(disjuncts.map(Not(_))))
+      case Not(And(conjuncts)) => apply(Or(conjuncts.map(Not(_))))
+      case Not(Imp(left, right)) => apply(And(Seq(left, Not(right))))
+      case Not(Eq(left, right)) => apply(Not(And(Seq(Imp(left, right), Imp(right, left)))))
+      case Or(disjuncts) => merge(disjuncts.map(d => apply(d)))
     }
   }
 
-  private def merge[T](clauseSets: Seq[Set[Clause[T]]]): Set[Clause[T]] = {
+  private def merge[T](clauseSets: Seq[Source[Clause[T], NotUsed]]): Source[Clause[T], NotUsed] = {
     if (clauseSets.length == 1)
       clauseSets.head
     else {
@@ -31,8 +28,8 @@ object toClauses {
     }
   }
 
-  private def merge[T](clauseSet1: Set[Clause[T]], clauseSet2: Set[Clause[T]]): Set[Clause[T]] = {
-    clauseSet1.flatMap( clause1 => clauseSet2.map( clause2 => merge(clause1, clause2)))
+  private def merge[T](clauseSet1: Source[Clause[T], NotUsed], clauseSet2: Source[Clause[T], NotUsed]): Source[Clause[T], NotUsed] = {
+    clauseSet1.flatMapMerge( 4, clause1 => clauseSet2.map( clause2 => merge(clause1, clause2)))
   }
 
   private def merge[T](clause1: Clause[T], clause2: Clause[T]): Clause[T] =
